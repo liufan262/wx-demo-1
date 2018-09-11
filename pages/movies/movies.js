3636a8d2db5dfbabb97b9ad7bf0751eaf60ad21a
liufan262 <liufan262@gmail.com>
2018-09-11 22:32:05 +0800

    获取数据

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,4 +1,7 @@
 // pages/movies/movies.js
+
+var app = getApp()
+
 Page({
 
   /**
@@ -12,18 +15,30 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-     wx.request({
-       url: 'http://t.yushu.im/v2/movie/top250',
-       data:'',
-       method: 'GET',
-       success: function(res){
-         console.log(res)
-       },
-       fail: function(){
-         console.log('调用失败')
-       }
-     })
+      var inTheatersUrl = app.globalData.doubanBase +
+          "/v2/movie/in_theaters";
+      var comingSoonUrl = app.globalData.doubanBase +
+          "/v2/movie/coming_soon";
+      var top250Url = app.globalData.doubanBase +
+          "/v2/movie/top250" ;
+
+      this.getMovieListData(inTheatersUrl)
+      this.getMovieListData(comingSoonUrl)
+      this.getMovieListData(top250Url)
   },
 
 
+    getMovieListData:function(url){
+        wx.request({
+            url: url,
+            data: '',
+            method: 'GET',
+            success: function (res) {
+                console.log(res)
+            },
+            fail: function () {
+                console.log('调用失败')
+            }
+        })
+    }
 })
