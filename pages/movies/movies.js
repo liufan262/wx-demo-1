3cec1da0bb1e479d14e4afed232e06b4735117a1
liufan262 <liufan262@gmail.com>
2018-10-09 23:19:27 +0800

    new

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -4,41 +4,69 @@ var app = getApp()
 
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        inTheaters: {},
+        comingSoon: {},
+        top250: {},
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-      var inTheatersUrl = app.globalData.doubanBase +
-          "/v2/movie/in_theaters";
-      var comingSoonUrl = app.globalData.doubanBase +
-          "/v2/movie/coming_soon";
-      var top250Url = app.globalData.doubanBase +
-          "/v2/movie/top250" ;
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        let that = this
 
-      this.getMovieListData(inTheatersUrl)
-      this.getMovieListData(comingSoonUrl)
-      this.getMovieListData(top250Url)
-  },
+        var inTheatersUrl = app.globalData.doubanBase +
+            "/v2/movie/in_theaters" + "?start=0&count=3";
+        var comingSoonUrl = app.globalData.doubanBase +
+            "/v2/movie/coming_soon" + "?start=0&count=3";
+        var top250Url = app.globalData.doubanBase +
+            "/v2/movie/top250" + "?start=0&count=3";
 
+        this.getMovieListData(inTheatersUrl, "inTheaters", "正在热映");
+        this.getMovieListData(comingSoonUrl, "comingSoon", "即将上映");
+        this.getMovieListData(top250Url, "top250", "豆瓣Top250");
+    },
 
-    getMovieListData:function(url){
+    getMovieListData: function (url, settedKey) {
+        let that = this
         wx.request({
             url: url,
             data: '',
             method: 'GET',
-            success: function (res) {
+            success: function(res) {
                 console.log(res)
+                that.processDoubanData(res.data, settedKey)
             },
-            fail: function () {
+            fail: function() {
                 console.log('调用失败')
             }
         })
+    },
+    processDoubanData: function (moviesDouban, settedKey) {
+        var movies = [];
+        for (var idx in moviesDouban.subjects) {
+            var subject = moviesDouban.subjects[idx];
+            var title = subject.title;
+            if (title.length >= 6) {
+                title = title.substring(0, 6) + "...";
+            }
+            var temp = {
+                title: title,
+                average: subject.rating.average,
+                coverageUrl: subject.images.large,
+                movieId: subject.id
+            }
+            movies.push(temp)
+        }
+        var readyData = {};
+        readyData[settedKey] = {
+            movies: movies
+        }
+        this.setData(readyData);
+        console.log(this.data)
     }
 })
